d1bbbf48d6582f93a1f6a309a9607dc678e28074
akira2542 <akiraakamura@gmail.com>
2024-10-25 21:08:33 +0700

    update repo

--- src/repositories/your-entity.repository.ts
+++ src/repositories/your-entity.repository.ts
@@ -1,13 +1,18 @@
 // repositories/YourEntityRepository.ts
-import { EntityManager, Repository } from 'typeorm';
+import { EntityManager, Like, Repository } from 'typeorm';
 import { YourEntity } from '../entities/your-entity.entity';
-
+import { Injectable } from '@nestjs/common';
+@Injectable()
 export class YourEntityRepository extends Repository<YourEntity> {
     constructor(private entityManager: EntityManager) {
         super(YourEntity, entityManager);
     }
 
-    findAll() {
-        this.find();
+    findByName(name: string) {
+        this.find({
+            where: {
+                name: Like(`%${name}%`)
+            }
+        });
     }
 }
